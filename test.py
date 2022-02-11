791ec0d50f5eb054952a06e8368b1cc977d1255d
Ryan P Sulapas <ryansulapas@gmail.com>
2022-02-11 15:58:16 -0600

    Rev 1 of website
    Working player select

--- test.py
+++ test.py
@@ -3,12 +3,14 @@ import pandas as pd
 import numpy as np
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Connection
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import inspect
 import psycopg2
 import pandas as pd
 import requests
 import streamlit.components.v1 as components
+import plotly.express as px
 
 class Tweet(object):
     def __init__(self, s, embed_str=False):
@@ -27,47 +29,152 @@ class Tweet(object):
     def component(self):
         return components.html(self.text, height=600)
 
-st.title('NBAKlout')
+st.image("logo.png")
 
 
 #DATE_COLUMN = 'date/time'
 #DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 #            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-#@st.cache
-#def load_data(nrows):
-#    data = pd.read_csv(DATA_URL, nrows=nrows)
-#    lowercase = lambda x: str(x).lower()
-#    data.rename(lowercase, axis='columns', inplace=True)
-#    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-#    return data
 
+@st.cache(allow_output_mutation=True)
+def get_connection(path):
+    """Put the connection in cache to reuse if path does not change."""
+    engine = create_engine(path)
+    return engine.connect()
+
+@st.cache(allow_output_mutation=True, hash_funcs={Connection: id})
+def get_data(conn):
+    query = 'select * from demo;'
+    df = pd.read_sql(query,conn)
+    return df
 #Postgres connect
-player = st.sidebar.text_input("Enter Player")
-stat1 = st.sidebar.text_input("Enter Stat")
-stat2 = st.sidebar.text_input("Enter Stat: Per Game default")
-data_load_state = st.text('Loading data...')
 instance = st.secrets["instance"]
 dbname = st.secrets["dbname"]
 username = st.secrets["username"]
 passwd = st.secrets["passwd"]
 hostname = st.secrets["hostname"]
 portnum = st.secrets["portnum"]
-engine = create_engine("postgresql://{}:{}@{}:{}/{}".format(username,passwd,hostname,portnum,dbname))
-session = scoped_session(sessionmaker(bind=engine))
-inspector = inspect(engine)
-conn = engine.connect()
-query = 'select * from public."Salary";'
-df = pd.read_sql(query,conn)
-st.dataframe(df)
+path = "postgresql://{}:{}@{}:{}/{}".format(username,passwd,hostname,portnum,dbname)
 
-st.text("Top 5")
-df['max_rank'] = df['2021-22'].rank(method='max')
-st.dataframe(df.sort_values(by=['max_rank'], ascending=False).head(5))
-st.text("Bottom 5")
-st.dataframe(df.sort_values(by=['max_rank']).tail(5))
+playerlist = ["All"]
+statlist = ["None"]
+#load data
+spin = st.spinner('Loading data...')
+stats = pd.DataFrame()
+with spin:
+    engine = get_connection(path)
+    df = get_data(engine)
+    stats = df.copy().sort_values(by='name')
+    playerlist.extend(stats.set_index('name').index.to_list())
 
-data_load_state.text("Stats loaded")
+
+#stats output
+playername = st.sidebar.selectbox("Enter Player", playerlist)
+stat1 = st.sidebar.text_input("Enter Stat")
+stat2 = st.sidebar.text_input("Enter Stat: Per Game default")
+stats = stats.set_index('name')
+stats['rk'] = stats['followers per dollar'].rank(method='first',ascending=False)
+stats['stat1_rk'] = stats['twit_followers_count'].rank(method='first',ascending=False)
+stats['stat2_rk'] = stats['2021-22'].rank(method='first',ascending=False)
+if playername != "All":
+    #st.dataframe(stats)
+    #statone = stats.sort_values
+    c1,c2,c3 = st.columns(3)
+    with c2:
+        st.image("imgs/{}.jpg".format(stats.loc[playername]['br_name']))
+        st.write(playername)
+
+    d1,d2,d3 = st.columns(3)
+    with d1:
+        st.metric("Followers",stats.loc[playername]['twit_followers_count'])
+    with d2:
+        st.metric("Followers/$",stats.loc[playername]['followers per dollar'])
+    with d3:
+        st.metric("Salary",stats.loc[playername]['2021-22'])
+
+    st.markdown("<h1 style='text-align: center; color: red;'>Similar Followers</h1>", unsafe_allow_html=True)
+
+    s1 = ""
+    s2 = ""
+    if stats.loc[playername]['stat1_rk'] == 1:
+        s1 = stats.index[stats['stat1_rk']==2][0]
+        s2 = stats.index[stats['stat1_rk']==3][0]
+    else:
+        ix = stats.loc[playername]['stat1_rk']
+        s1 = stats.index[stats['stat1_rk']==ix+1][0]
+        s2 = stats.index[stats['stat1_rk']==ix-1][0]
+    e1,e2 = st.columns(2)
+    with e1:
+        st.image("imgs/{}.jpg".format(stats.loc[s1]['br_name']))
+        st.write(s1)
+        st.write("Followers: {}".format(stats.loc[s1]['twit_followers_count']))
+        st.write("Salary: {}".format(stats.loc[s1]['2021-22']))
+        st.write("Followers/$: {}".format(stats.loc[s1]['followers per dollar']))
+    with e2:
+        st.image("imgs/{}.jpg".format(stats.loc[s2]['br_name']))
+        st.write(s2)
+        st.write("Followers: {}".format(stats.loc[s2]['twit_followers_count']))
+        st.write("Salary: {}".format(stats.loc[s2]['2021-22']))
+        st.write("Followers/$: {}".format(stats.loc[s2]['followers per dollar']))
+    st.markdown("<h1 style='text-align: center; color: red;'>Similar Salary</h1>", unsafe_allow_html=True)
+    t1 = ""
+    t2 = ""
+    if stats.loc[playername]['stat2_rk'] == 1:
+        t1 = stats.index[stats['stat2_rk']==2][0]
+        t2 = stats.index[stats['stat2_rk']==3][0]
+    else:
+        ix = stats.loc[playername]['stat2_rk']
+        t1 = stats.index[stats['stat2_rk']==ix+1][0]
+        t2 = stats.index[stats['stat2_rk']==ix-1][0]
+    f1,f2 = st.columns(2)
+    with f1:
+        st.image("imgs/{}.jpg".format(stats.loc[t1]['br_name']))
+        st.write(t1)
+        st.write("Followers: {}".format(stats.loc[t1]['twit_followers_count']))
+        st.write("Salary: {}".format(stats.loc[t1]['2021-22']))
+        st.write("Followers/$: {}".format(stats.loc[t1]['followers per dollar']))
+    with f2:
+        st.image("imgs/{}.jpg".format(stats.loc[t2]['br_name']))
+        st.write(t2)
+        st.write("Followers: {}".format(stats.loc[t2]['twit_followers_count']))
+        st.write("Salary: {}".format(stats.loc[t2]['2021-22']))
+        st.write("Followers/$: {}".format(stats.loc[t2]['followers per dollar']))
+
+else:
+    #st.dataframe(stats)
+    with st.spinner('Loading data...'):
+        fig = (px.scatter_3d(df, x='2021-22', y='twit_followers_count',
+             hover_name='name',z='followers per dollar',
+             color = 'followers per dollar', color_continuous_scale ="rainbow"))
+        st.plotly_chart(fig,use_container_width=True)
+    st.markdown("<h1 style='text-align: center; color: red;'>Top 3</h1>", unsafe_allow_html=True)
+    g1, g2, g3 = st.columns(3)
+    s1 = stats.index[stats['rk']==1][0]
+    s2 = stats.index[stats['rk']==2][0]
+    s3 = stats.index[stats['rk']==3][0]
+    with g1:
+        st.image("imgs/{}.jpg".format(stats.loc[s1]['br_name']))
+        st.write(s1)
+        st.write("Followers: {}".format(stats.loc[s1]['twit_followers_count']))
+        st.write("Salary: {}".format(stats.loc[s1]['2021-22']))
+        st.write("Followers/$: {}".format(stats.loc[s1]['followers per dollar']))
+    with g2:
+        st.image("imgs/{}.jpg".format(stats.loc[s2]['br_name']))
+        st.write(s2)
+        st.write("Followers: {}".format(stats.loc[s2]['twit_followers_count']))
+        st.write("Salary: {}".format(stats.loc[s2]['2021-22']))
+        st.write("Followers/$: {}".format(stats.loc[s2]['followers per dollar']))
+    with g3:
+        st.image("imgs/{}.jpg".format(stats.loc[s3]['br_name']))
+        st.write(s3)
+        st.write("Followers: {}".format(stats.loc[s3]['twit_followers_count']))
+        st.write("Salary: {}".format(stats.loc[s3]['2021-22']))
+        st.write("Followers/$: {}".format(stats.loc[s3]['followers per dollar']))
+
+
+
+#loaded
 
 t = Tweet("https://twitter.com/NBA/status/1491541849617612806").component()
 #data_load_state = st.text('Loading data...')
