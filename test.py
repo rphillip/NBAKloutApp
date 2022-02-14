abba270f83d7ba58b09201327db14d6b8da99a70
Ryan P Sulapas <ryansulapas@gmail.com>
2022-02-14 14:15:24 -0600

    Working stats
    Stat fields updated so they are now working... relatively.

--- test.py
+++ test.py
@@ -46,8 +46,39 @@ def get_connection(path):
 @st.cache(allow_output_mutation=True, hash_funcs={Connection: id})
 def get_data(conn):
     query = 'select * from demo;'
+    query2 = 'select name from stats;'
+    query3 = 'select * from defs;'
+    #query4 = 'select * from stats;'
     df = pd.read_sql(query,conn)
-    return df
+    df2 = pd.read_sql(query2,conn)
+    df3 = pd.read_sql(query3,conn)
+    #df4 = pd.read_sql(query4,conn)
+    return (df, df2, df3)
+
+def get_table(conn, s1, s2, op):
+    if s1 == 1:
+        query = 'select name, br_name, {} from stats;'.format(s1)
+        return pd.read_sql(query,conn)
+    elif s2 == 1:
+        query = 'select name, br_name, {} from stats;'.format(s2)
+        return pd.read_sql(query,conn)
+    else:
+        query = 'select name, br_name, {}, {} from stats;'.format(s1,s2)
+        df = pd.read_sql(query,conn)
+        s3 = ""
+        if op == 1:
+            s3 = s1 + '*' + s2
+            df[s3] = df[s1] * df[s2]
+        elif op == 2:
+            s3 = s1 + '/' + s2
+            df[s3] = df[s1] / df[s2]
+            df.loc[np.isinf(df[s3]), s3] = np.nan
+
+        df['rk'] = df[s3].rank(method='first',ascending=False)
+        df[s1+'_rk'] = df[s1].rank(method='first',ascending=False)
+        df[s2+'_rk'] = df[s3].rank(method='first',ascending=False)
+        return df
+
 #Postgres connect
 instance = st.secrets["instance"]
 dbname = st.secrets["dbname"]
@@ -59,27 +90,55 @@ path = "postgresql://{}:{}@{}:{}/{}".format(username,passwd,hostname,portnum,dbn
 
 playerlist = ["All"]
 statlist = ["None"]
+statdf = pd.DataFrame()
 #load data
 spin = st.spinner('Loading data...')
 stats = pd.DataFrame()
 with spin:
     engine = get_connection(path)
     df = get_data(engine)
-    stats = df.copy().sort_values(by='name')
-    playerlist.extend(stats.set_index('name').index.to_list())
+    stats = df[0].copy().sort_values(by='name')
+    playerlist.extend(df[1]['name'].values.tolist())
+    statlist.extend(df[2]['desc'].values.tolist())
+    statdf = df[2].set_index('desc')
 
-
-#stats output
+op = 0
 playername = st.sidebar.selectbox("Enter Player", playerlist)
-stat1 = st.sidebar.text_input("Enter Stat")
-stat2 = st.sidebar.text_input("Enter Stat: Per Game default")
-stats = stats.set_index('name')
-stats['rk'] = stats['followers per dollar'].rank(method='first',ascending=False)
-stats['stat1_rk'] = stats['twit_followers_count'].rank(method='first',ascending=False)
-stats['stat2_rk'] = stats['2021-22'].rank(method='first',ascending=False)
+stat1 = st.sidebar.selectbox("Stat One", statlist, index=1)
+operation = st.sidebar.radio("Operation", ('multiply by','divide by'),index = 1)
+if operation == 'multiply by':
+    op = 1
+elif operation == 'divide by':
+    op = 2
+stat2 = st.sidebar.selectbox("Stat Two", statlist, index = statlist.index('Dollars: Salary'))
+
+stats = pd.DataFrame()
+sql1 =statdf.loc[stat1]['0']
+sql2 =statdf.loc[stat2]['0']
+if op == 2:
+    sql3 = sql1 + '/' + sql2
+    stat3 = stat1 +'/' + stat2
+else:
+    sql3 = sql1 + '*' + sql2
+    stat3 = stat1 +'*' + stat2
+with st.spinner('Loading data...'):
+    if (stat1 == 'All') and (stat1 == 'All'):
+        stats = stats
+    elif stat1 == 'All':
+        stats = get_table(engine,1,sql2,op)
+    elif stat2 == 'All':
+        stats = get_table(engine,sql1,1,op)
+    else:
+        stats = get_table(engine,sql1,sql2,op)
+
+
+#stats['rk'] = stats['followers per dollar'].rank(method='first',ascending=False)
+#stats['stat1_rk'] = stats['twit_followers_count'].rank(method='first',ascending=False)
+#stats['stat2_rk'] = stats['2021-22'].rank(method='first',ascending=False)
 if playername != "All":
     #st.dataframe(stats)
     #statone = stats.sort_values
+    stats = stats.set_index('name')
     c1,c2,c3 = st.columns(3)
     with c2:
         st.image("imgs/{}.jpg".format(stats.loc[playername]['br_name']))
@@ -87,68 +146,75 @@ if playername != "All":
 
     d1,d2,d3 = st.columns(3)
     with d1:
-        st.metric("Followers",stats.loc[playername]['twit_followers_count'])
+        st.metric(stat1,stats.loc[playername][sql1])
     with d2:
-        st.metric("Followers/$",stats.loc[playername]['followers per dollar'])
+        st.metric(stat3,stats.loc[playername][sql3])
     with d3:
-        st.metric("Salary",stats.loc[playername]['2021-22'])
+        st.metric(stat2,stats.loc[playername][sql2])
 
     st.markdown("<h1 style='text-align: center; color: red;'>Similar Followers</h1>", unsafe_allow_html=True)
 
     s1 = ""
     s2 = ""
-    if stats.loc[playername]['stat1_rk'] == 1:
-        s1 = stats.index[stats['stat1_rk']==2][0]
-        s2 = stats.index[stats['stat1_rk']==3][0]
+    if stats.loc[playername][sql1+'_rk'] == 1:
+        s1 = stats.index[stats[sql1+'_rk']==2][0]
+        s2 = stats.index[stats[sql1+'_rk']==3][0]
     else:
-        ix = stats.loc[playername]['stat1_rk']
-        s1 = stats.index[stats['stat1_rk']==ix+1][0]
-        s2 = stats.index[stats['stat1_rk']==ix-1][0]
+        ix = stats.loc[playername][sql1+'_rk']
+        s1 = stats.index[stats[sql1+'_rk']==ix+1][0]
+        s2 = stats.index[stats[sql1+'_rk']==ix-1][0]
     e1,e2 = st.columns(2)
     with e1:
         st.image("imgs/{}.jpg".format(stats.loc[s1]['br_name']))
         st.write(s1)
-        st.write("Followers: {}".format(stats.loc[s1]['twit_followers_count']))
-        st.write("Salary: {}".format(stats.loc[s1]['2021-22']))
-        st.write("Followers/$: {}".format(stats.loc[s1]['followers per dollar']))
+        st.write("{}: {}".format(stat1,stats.loc[s1][sql1]))
+        st.write("{}: {}".format(stat2,stats.loc[s1][sql2]))
+        st.write("{}: {}".format(stat3,stats.loc[s1][sql3]))
     with e2:
         st.image("imgs/{}.jpg".format(stats.loc[s2]['br_name']))
         st.write(s2)
-        st.write("Followers: {}".format(stats.loc[s2]['twit_followers_count']))
-        st.write("Salary: {}".format(stats.loc[s2]['2021-22']))
-        st.write("Followers/$: {}".format(stats.loc[s2]['followers per dollar']))
+        st.write("{}: {}".format(stat1,stats.loc[s2][sql1]))
+        st.write("{}: {}".format(stat2,stats.loc[s2][sql2]))
+        st.write("{}: {}".format(stat3,stats.loc[s2][sql3]))
     st.markdown("<h1 style='text-align: center; color: red;'>Similar Salary</h1>", unsafe_allow_html=True)
     t1 = ""
     t2 = ""
-    if stats.loc[playername]['stat2_rk'] == 1:
-        t1 = stats.index[stats['stat2_rk']==2][0]
-        t2 = stats.index[stats['stat2_rk']==3][0]
+    if stats.loc[playername][sql2+'_rk'] == 1:
+        t1 = stats.index[stats[sql2+'_rk']==2][0]
+        t2 = stats.index[stats[sql2+'_rk']==3][0]
     else:
-        ix = stats.loc[playername]['stat2_rk']
-        t1 = stats.index[stats['stat2_rk']==ix+1][0]
-        t2 = stats.index[stats['stat2_rk']==ix-1][0]
+        ix = stats.loc[playername][sql2+'_rk']
+        t1 = stats.index[stats[sql2+'_rk']==ix+1][0]
+        t2 = stats.index[stats[sql2+'_rk']==ix-1][0]
     f1,f2 = st.columns(2)
     with f1:
         st.image("imgs/{}.jpg".format(stats.loc[t1]['br_name']))
-        st.write(t1)
-        st.write("Followers: {}".format(stats.loc[t1]['twit_followers_count']))
-        st.write("Salary: {}".format(stats.loc[t1]['2021-22']))
-        st.write("Followers/$: {}".format(stats.loc[t1]['followers per dollar']))
+        st.write(s1)
+        st.write("{}: {}".format(stat1,stats.loc[t1][sql1]))
+        st.write("{}: {}".format(stat2,stats.loc[t1][sql2]))
+        st.write("{}: {}".format(stat3,stats.loc[t1][sql3]))
     with f2:
         st.image("imgs/{}.jpg".format(stats.loc[t2]['br_name']))
-        st.write(t2)
-        st.write("Followers: {}".format(stats.loc[t2]['twit_followers_count']))
-        st.write("Salary: {}".format(stats.loc[t2]['2021-22']))
-        st.write("Followers/$: {}".format(stats.loc[t2]['followers per dollar']))
+        st.write(s2)
+        st.write("{}: {}".format(stat1,stats.loc[t2][sql1]))
+        st.write("{}: {}".format(stat2,stats.loc[t2][sql2]))
+        st.write("{}: {}".format(stat3,stats.loc[t2][sql3]))
 
 else:
     #st.dataframe(stats)
     with st.spinner('Loading data...'):
-        fig = (px.scatter_3d(df, x='2021-22', y='twit_followers_count',
-             hover_name='name',z='followers per dollar',
-             color = 'followers per dollar', color_continuous_scale ="rainbow"))
-        st.plotly_chart(fig,use_container_width=True)
+        fig = (px.scatter_3d(stats, x=sql1, y=sql2,
+             hover_name='name',z=sql3,
+             color = sql3, color_continuous_scale ="rainbow",
+             labels={
+                sql1: stat1,
+                sql2: stat2,
+                sql3: stat3
+                },
+             title=stat3))
+        st.plotly_chart(fig,use_container_width=False)
     st.markdown("<h1 style='text-align: center; color: red;'>Top 3</h1>", unsafe_allow_html=True)
+    stats = stats.set_index('name')
     g1, g2, g3 = st.columns(3)
     s1 = stats.index[stats['rk']==1][0]
     s2 = stats.index[stats['rk']==2][0]
@@ -156,27 +222,29 @@ else:
     with g1:
         st.image("imgs/{}.jpg".format(stats.loc[s1]['br_name']))
         st.write(s1)
-        st.write("Followers: {}".format(stats.loc[s1]['twit_followers_count']))
-        st.write("Salary: {}".format(stats.loc[s1]['2021-22']))
-        st.write("Followers/$: {}".format(stats.loc[s1]['followers per dollar']))
+        st.write("{}: {}".format(stat1,stats.loc[s1][sql1]))
+        st.write("{}: {}".format(stat2,stats.loc[s1][sql2]))
+        st.write("{}: {}".format(stat3,stats.loc[s1][sql3]))
     with g2:
         st.image("imgs/{}.jpg".format(stats.loc[s2]['br_name']))
         st.write(s2)
-        st.write("Followers: {}".format(stats.loc[s2]['twit_followers_count']))
-        st.write("Salary: {}".format(stats.loc[s2]['2021-22']))
-        st.write("Followers/$: {}".format(stats.loc[s2]['followers per dollar']))
+        st.write("{}: {}".format(stat1,stats.loc[s2][sql1]))
+        st.write("{}: {}".format(stat2,stats.loc[s2][sql2]))
+        st.write("{}: {}".format(stat3,stats.loc[s2][sql3]))
     with g3:
         st.image("imgs/{}.jpg".format(stats.loc[s3]['br_name']))
         st.write(s3)
-        st.write("Followers: {}".format(stats.loc[s3]['twit_followers_count']))
-        st.write("Salary: {}".format(stats.loc[s3]['2021-22']))
-        st.write("Followers/$: {}".format(stats.loc[s3]['followers per dollar']))
+        st.write("{}: {}".format(stat1,stats.loc[s3][sql1]))
+        st.write("{}: {}".format(stat2,stats.loc[s3][sql2]))
+        st.write("{}: {}".format(stat3,stats.loc[s3][sql3]))
 
 
 
 #loaded
 
 t = Tweet("https://twitter.com/NBA/status/1491541849617612806").component()
+st.dataframe(stats)
+
 #data_load_state = st.text('Loading data...')
 #data = load_data(10000)
 #data_load_state.text("Done! (using st.cache)")
