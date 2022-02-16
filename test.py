20e2604d1fbb1105ff7a6f096e942da36ff965f9
Ryan P Sulapas <ryansulapas@gmail.com>
2022-02-16 12:10:21 -0600

    Update test.py

--- test.py
+++ test.py
@@ -103,7 +103,6 @@ class Player(object):
         met = None
         st.image("imgs/{}.jpg".format(self.br_name))
         st.subheader(self.name)
-        st.write(len(stat))
         if len(stat) == 1:
             met = self.values[0]
             if other is not None:
